2c206b7a9ff920aaa1d3d64556a8e5a375c6c8c2
testerlingSmidge <github@smidgeoftrauma.com>
2024-08-06 03:41:25 -0700

    Update trigger.js

--- api/trigger.js
+++ api/trigger.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
+
 const path = require('path');
 const fs = require('fs');
 
@@ -9,13 +8,6 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(express.static('public'));
-
-// Rate Limiting
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
-});
-app.use(limiter);
 
 // Endpoint to serve modified HTML
 app.get('/trigger', (req, res) => {
