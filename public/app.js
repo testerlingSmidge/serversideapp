18cbf8f6f48d6856cdf0316b8ff60e25231e289b
testerlingSmidge <github@smidgeoftrauma.com>
2024-08-06 04:39:29 -0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         })
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
+                return response.text().then(text => { throw new Error(text) });
             }
             return response.json();
         })
