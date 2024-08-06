2235203aa553774844b9e72eb40f85da1f37862c
testerlingSmidge <github@smidgeoftrauma.com>
2024-08-06 03:03:11 -0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -6,8 +6,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
     triggerGong.addEventListener('click', () => {
         var targetId = document.getElementById('targetId');
         
+        // Update fetch URL if your server is running on a different URL
         fetch('https://serversideapp-xi.vercel.app/trigger')
-            .then(response => response.json())
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.json();
+            })
             .then(data => {
                 if (data.status === "triggered") {
                     targetId.innerHTML = data.value;
